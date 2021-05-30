0ae2eae9db9348dd6a70b9c74a8f0062431f3ece
Tony Le <le.tony.05@gmail.com>
2021-05-29 21:54:41 -0400

    Fix bot commands and add playing activity to show help command

--- bot.js
+++ bot.js
@@ -9,73 +9,88 @@ const prefix = '%'
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user
+      .setActivity(prefix + "help", { type: "PLAYING" })
+      .then((presence) =>
+        console.log(`Activity set to ${presence.activities[0].name}`)
+      )
+      .catch(console.error);
 });
 
 client.on('message', message => {
     // Join the same voice channel of the author of the message
-    if (prefix + message.content.toLowerCase() in voice_commands) {
-        if (message.member.voice.channel) {
-            const connection = message.member.voice.channel.join().then((connection) => {
-                const command = message.content;
-                const commandWithoutPrefix = command.substr(
-                  Math.max(0, command.indexOf(prefix) + 1),
-                  command.length
-                );
-                const audio = voice_commands[commandWithoutPrefix].audio;
-                const picture = voice_commands[commandWithoutPrefix].picture;
+    console.log('message detected');
+    const command = message.content;
+    const commandWithoutPrefix = command.substr(
+      Math.max(0, command.indexOf(prefix) + 1),
+      command.length
+    );
+    console.log(command);
+    console.log(commandWithoutPrefix);
+    if (commandWithoutPrefix in voice_commands) {
+      if (message.member.voice.channel) {
+        const connection = message.member.voice.channel
+          .join()
+          .then((connection) => {
+            const audio = voice_commands[commandWithoutPrefix].audio;
+            const picture = voice_commands[commandWithoutPrefix].picture;
 
-                const dispatcher = connection.play(audio, { volume: 0.5 });
-                let start = 0;
-                let end = 0;
+            const dispatcher = connection.play(audio, { volume: 0.5 });
+            let start = 0;
+            let end = 0;
 
-                dispatcher.on('start', () => {
-                    start = new Date();
-                    start = start.getTime();
-                    console.log("Playing");
-                });
-
-                dispatcher.on('debug', (info) => {
-                    console.log("Debug: " + info);
-                });
-
-                dispatcher.on('finish', () => {
-                    console.log("Finished");
-                    end = new Date();
-                    end = end.getTime();
-                    console.log("Playtime: " + (end - start) + "ms");
-                    connection.disconnect();
-                });
-
-                dispatcher.on('error', console.error);
-
-                if (picture) {
-                    //Check if picture file exists
-                    fs.access(picture, fs.constants.R_OK, (err) => {
-                        if (err) {
-                            console.log(`${file} ${err ? 'is not readable' : 'is readable'}`);
-                        }
-                        else {
-                            // Send the attachment in the message channel with a content
-                            const attachment = new MessageAttachment(picture);
-                            message.channel.send(attachment);
-                        }
-                    });
-                }
+            dispatcher.on("start", () => {
+              start = new Date();
+              start = start.getTime();
+              console.log("Playing");
             });
-        }
-        else {
-            message.channel.send("Please join a voice channel.");
-        }
-    }
-    else if (message.content == '!help') {
-        let text = 'The list of available commands. Pre-pend with an \'' + prefix +'\' to run the command.' + prefix + '{command}:';
+
+            dispatcher.on("debug", (info) => {
+              console.log("Debug: " + info);
+            });
+
+            dispatcher.on("finish", () => {
+              console.log("Finished");
+              end = new Date();
+              end = end.getTime();
+              console.log("Playtime: " + (end - start) + "ms");
+              connection.disconnect();
+            });
+
+            dispatcher.on("error", console.error);
+
+            if (picture) {
+              //Check if picture file exists
+              fs.access(picture, fs.constants.R_OK, (err) => {
+                if (err) {
+                  console.log(
+                    `${file} ${err ? "is not readable" : "is readable"}`
+                  );
+                } else {
+                  // Send the attachment in the message channel with a content
+                  const attachment = new MessageAttachment(picture);
+                  message.channel.send(attachment);
+                }
+              });
+            }
+          });
+      } else {
+        message.channel.send("Please join a voice channel.");
+      }
+    } else if (message.content == prefix + "help") {
+      let text =
+        "The list of available commands. Pre-pend with an '" +
+        prefix +
+        "' to run the command. " +
+        prefix +
+        "{command}:";
+      text += "\n";
+      text += "help";
+      for (let command in voice_commands) {
         text += "\n";
-        text += "help";
-        for (command in voice_commands) {
-            text += "\n";
-            text += command;
-        }
-        message.channel.send(text);
+        text += command;
+      }
+      message.channel.send(text);
     }
 
 });
